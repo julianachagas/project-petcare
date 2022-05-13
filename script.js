980cfc77ab370af5f091b00712b3a5f6cb8af115
Juliana Chagas <julianavrchagas@gmail.com>
2022-05-13 00:10:07 -0300

    update onScroll function

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+onScroll()
+window.addEventListener('scroll', onScroll)
+
 function onScroll() {
-  let header = document.getElementById('header');
+  changeHeaderStyleOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function changeHeaderStyleOnScroll() {
+  let header = document.getElementById('header')
   if (scrollY > 0) {
     header.classList.add('scroll')
   } else {
@@ -7,16 +15,27 @@ function onScroll() {
   }
 }
 
-function openMenu(){
-  document.body.classList.add("menu-expanded");
+function showBackToTopButtonOnScroll() {
+  let backToTopButton = document.getElementById('backToTopButton')
+  if (scrollY > 500) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
 }
 
-function closeMenu(){
-  document.body.classList.remove("menu-expanded");
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
 }
 
 ScrollReveal({
-	origin: "top",
-	distance: "30px",
-	duration: 700,
-}).reveal("#home, #home .stat, #services header, #services .service-item, #about header, #about .content, #about .content img, #contact header, #contact .content, #contact .content img, footer")
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(
+  '#home, #home .stat, #services header, #services .service-item, #about header, #about .content, #about .content img, #contact header, #contact .content, #contact .content img, footer'
+)
